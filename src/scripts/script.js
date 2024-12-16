9efebbb28d4440b4f881255acb0f32a6466a3952
perkles <leoazevedo1450@gmail.com>
2024-12-16 00:13:04 -0300

    Tarefa: Criação da seção discover do site

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -38,3 +38,13 @@ ScrollReveal().reveal(".about__content .btn", {
     ...ScrollReveal10ption,
     delay: 900,
 });
+// Discover container
+ScrollReveal().reveal(".discover__card", {
+    ...ScrollReveal10ption,
+    interval: 500,
+})
+ScrollReveal().reveal(".discover__card__content", {
+    ...ScrollReveal10ption,
+    interval: 500,
+    delay: 300,
+})
